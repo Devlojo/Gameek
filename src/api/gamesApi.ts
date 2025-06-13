faae1c63c49d6374a87f2da121696dd1d6eb52d5
Devlojo <jonathan.lacouture@oclock.school>
2025-06-13 18:02:28 +0200

    activate api prod url

--- src/api/gamesApi.ts
+++ src/api/gamesApi.ts
@@ -4,9 +4,9 @@ import { gameSchema } from "@/types/index";
 export const getLatestReleasesGames = async () => {
   try {
     const { data: latestGames } = await axios.get(
-      /*`https://site--gameek-backend--bf7zj7wtgltq.code.run/games/latest-releases`,
-      { timeout: 5000 },*/
-      "http://localhost:8000/games/latest-releases",
+      `https://site--gameek-backend--bf7zj7wtgltq.code.run/games/latest-releases`,
+      { timeout: 5000 },
+      /*"http://localhost:8000/games/latest-releases",*/
     );
 
     const latestGamesParsed = gameSchema.parse(latestGames);
@@ -21,9 +21,9 @@ export const getLatestReleasesGames = async () => {
 export const getBestGames = async () => {
   try {
     const { data: bestGames } = await axios.get(
-      /*`https://site--gameek-backend--bf7zj7wtgltq.code.run/games/best`,
-      { timeout: 5000 },*/
-      "http://localhost:8000/games/best",
+      `https://site--gameek-backend--bf7zj7wtgltq.code.run/games/best`,
+      { timeout: 5000 },
+      // "http://localhost:8000/games/best",
     );
     const bestGamesParsed = gameSchema.parse(bestGames);
     return bestGamesParsed;
@@ -36,9 +36,9 @@ export const getBestGames = async () => {
 export const getLatestReviews = async () => {
   try {
     const { data: latestReviews } = await axios.get(
-      /*`https://site--gameek-backend--bf7zj7wtgltq.code.run/games/latest-reviews`,
-      { timeout: 5000 },*/
-      "http://localhost:8000/games/latest-reviews",
+      `https://site--gameek-backend--bf7zj7wtgltq.code.run/games/latest-reviews`,
+      { timeout: 5000 },
+      //"http://localhost:8000/games/latest-reviews",
     );
     const latestReviewsParsed = gameSchema.parse(latestReviews);
     return latestReviewsParsed;
