1b42a60b0bb4b7ce4a5c2ed1d1c40b8c980e6553
Devlojo <jonathan.lacouture@oclock.school>
2025-07-08 09:50:00 +0200

    text on bold on hover game card

--- src/components/ui/GameHoverCard.tsx
+++ src/components/ui/GameHoverCard.tsx
@@ -10,7 +10,7 @@ type TGame = {
 export const GameHoverCard = ({ platforms, genres, info }: TGame) => {
   return (
     <div className="inset-0 hidden flex-col items-center justify-center gap-3 rounded-md bg-global/75 opacity-0 transition duration-200 ease-in-out group-hover:opacity-100 lg:absolute lg:flex">
-      <p className="text-customWhite">{info}</p>
+      <p className="font-bold text-customWhite">{info}</p>
       <div className="flex flex-wrap justify-center gap-2">
         {platforms?.map((platform, index) => (
           <PlatformBadge platform={platform.platform.name} key={index} />
