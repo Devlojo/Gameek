1f02fbefae0cbd1ef3cc03772a10199b324de38a
Devlojo <jonathan.lacouture@oclock.school>
2025-09-30 16:18:54 +0200

    fixed order in useReviewsQuery at queryKey

--- src/queries/useReviewsQuery.ts
+++ src/queries/useReviewsQuery.ts
@@ -74,7 +74,7 @@ export const useReviewsByFilter = (
     isSuccess,
     isError,
   } = useQuery({
-    queryKey: ["reviewsFiltered", page, grade, genres, platforms, reviewer],
+    queryKey: ["reviewsFiltered", page, reviewer, genres, platforms, grade],
     queryFn: () => getAllReviews(page, reviewer, genres, platforms, grade),
     retry: false,
   });
